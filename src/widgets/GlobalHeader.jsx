480b38c3573d4e4768432db5686edd1dbe5bacc5
Sebi <sebim254@gmail.com>
2022-01-15 02:04:10 +0100

    implemented Settings.jsx

---
+++ src/widgets/GlobalHeader.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {ThemePreset} from "../theme/ThemePreset";
+import {Themes} from "../theme/ColorThemes";
+import {Icon} from "react-native-elements";
+import Widget from "../components/Widget";
+import {navigateTo} from "../tools/helpers";
+import {useNavigation, useRoute} from "@react-navigation/native";
+
+
+export default function GlobalHeader() {
+    const {theme, globalStyles, localStyles} = ThemePreset(createStyles);
+
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    return (
+        <View style={[globalStyles.row, localStyles.headerButtonContainer]}>
+            <TouchableOpacity
+                style={localStyles.headerButton}
+                onPress={() => navigation.navigate('Settings', {...route.params})}>
+                <Icon name="settings" color={theme.colors.onSurface} />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const createStyles = (theme = Themes.light) =>
+    StyleSheet.create({
+        headerButtonContainer: {
+          marginEnd: 8
+        },
+        headerButton: {
+            padding: 8,
+        }
+    });
