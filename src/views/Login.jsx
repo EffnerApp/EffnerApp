154ce408688e88fdaef9e3bd5fbbf5fb146d4f51
sebiIO <sebim254@gmail.com>
2022-01-18 21:57:00 +0100

    setup push notifications for android

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,15 @@
 import React, {useEffect, useState} from "react";
 
-import {ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from "react-native";
 import {ThemePreset} from "../theme/ThemePreset";
 import {Themes} from "../theme/ColorThemes";
 import Button from "../components/Button";
@@ -28,7 +37,7 @@ export default function LoginScreen({navigation, route}) {
     const [sClass, setClass] = useState();
 
     useEffect(() => {
-        if(error) {
+        if (error) {
             showToast('Error', error, 'error');
         }
 
@@ -56,55 +65,57 @@ export default function LoginScreen({navigation, route}) {
     }
 
     return (
-        <View style={globalStyles.screen}>
-            <ScrollView style={globalStyles.content}>
-                <View style={localStyles.logoOuterContainer}>
-                    <View style={localStyles.logoContainer}>
-                        <SvgXml style={{alignSelf: "center"}} xml={logo} width={100} height={100}/>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <View style={globalStyles.screen}>
+                <ScrollView style={globalStyles.content}>
+                    <View style={localStyles.logoOuterContainer}>
+                        <View style={localStyles.logoContainer}>
+                            <SvgXml style={{alignSelf: "center"}} xml={logo} width={100} height={100}/>
+                        </View>
                     </View>
-                </View>
+
+                    <View style={[globalStyles.box, globalStyles.dropShadow]}>
+                        <Text style={globalStyles.text}>Login</Text>
+                        <TextInput onChangeText={setId}
+                                   style={[globalStyles.box, localStyles.boxSecondary, globalStyles.dropShadow, globalStyles.mt15]}
+                                   keyboardAppearance={theme.keyboardAppearance}
+                                   placeholder="ID"
+                                   keyboardType="numeric"
+                                   placeholderTextColor={theme.colors.onSecondary}
+                        />
+                        <TextInput onChangeText={setPassword}
+                                   style={[globalStyles.box, localStyles.boxSecondary, globalStyles.dropShadow]}
+                                   keyboardAppearance={theme.keyboardAppearance}
+                                   placeholder="Password"
+                                   secureTextEntry={true}
+                                   placeholderTextColor={theme.colors.onSecondary}
+                        />
+                        <View style={[globalStyles.box, localStyles.boxSecondary, globalStyles.dropShadow]}>
+                            <Picker
+                                style={{color: theme.colors.onPrimary}}
+                                dropdownIconColor={theme.colors.onPrimary}
+                                selectedValue={sClass}
+                                onValueChange={(value) => setClass(value)}>
+                                {classes.map((c, i) => <Picker.Item key={i} label={c} value={c}
+                                                                    color={!runsOn('android') ? theme.colors.font : null}/>)}
+                            </Picker>
+                        </View>
+                        <Button icon="east" title="Login" overrideStyles={[localStyles.boxPrimary, globalStyles.mt30]}
+                                onPress={performLogin} running={running}/>
+                    </View>
+                </ScrollView>
                 <View style={[globalStyles.box, globalStyles.dropShadow]}>
-                    <Text style={globalStyles.text}>Login</Text>
-                    <TextInput onChangeText={setId}
-                               style={[globalStyles.box, localStyles.boxSecondary, globalStyles.dropShadow, globalStyles.mt15]}
-                               keyboardAppearance={theme.keyboardAppearance}
-                               placeholder="ID"
-                               keyboardType="numeric"
-                               placeholderTextColor={theme.colors.onSecondary}
-                    />
-                    <TextInput onChangeText={setPassword}
-                               style={[globalStyles.box, localStyles.boxSecondary, globalStyles.dropShadow]}
-                               keyboardAppearance={theme.keyboardAppearance}
-                               placeholder="Password"
-                               secureTextEntry={true}
-                               placeholderTextColor={theme.colors.onSecondary}
-                    />
-                    <View style={[globalStyles.box, localStyles.boxSecondary, globalStyles.dropShadow]}>
-                        <Picker
-                            style={{color: theme.colors.onPrimary}}
-                            dropdownIconColor={theme.colors.onPrimary}
-                            selectedValue={sClass}
-                            onValueChange={(value) => setClass(value)}>
-                            {classes.map((c, i) => <Picker.Item key={i} label={c} value={c}
-                                                                color={!runsOn('android') ? theme.colors.font : null}/>)}
-                        </Picker>
+                    <View style={globalStyles.row}>
+                        <TouchableOpacity onPress={() => openUri('https://status.effner.app')}><Text
+                            style={[globalStyles.text, {paddingHorizontal: 10}]}>Status</Text></TouchableOpacity>
+                        <TouchableOpacity onPress={() => openUri(`${BASE_URL_GO}/imprint`, {type: 'pdf'})}><Text
+                            style={[globalStyles.text, {paddingHorizontal: 10}]}>Impressum</Text></TouchableOpacity>
+                        <TouchableOpacity onPress={() => openUri(`${BASE_URL_GO}/privacy`, {type: 'pdf'})}><Text
+                            style={[globalStyles.text, {paddingHorizontal: 10}]}>Datenschutzerklärung</Text></TouchableOpacity>
                     </View>
-                    <Button icon="east" title="Login" overrideStyles={[localStyles.boxPrimary, globalStyles.mt30]}
-                            onPress={performLogin} running={running}/>
-                </View>
-            </ScrollView>
-            <View style={[globalStyles.box, globalStyles.dropShadow]}>
-                <View style={globalStyles.row}>
-                    <TouchableOpacity onPress={() => openUri('https://status.effner.app')}><Text
-                        style={[globalStyles.text, {paddingHorizontal: 10}]}>Status</Text></TouchableOpacity>
-                    <TouchableOpacity onPress={() => openUri(`${BASE_URL_GO}/imprint`, {type: 'pdf'})}><Text
-                        style={[globalStyles.text, {paddingHorizontal: 10}]}>Impressum</Text></TouchableOpacity>
-                    <TouchableOpacity onPress={() => openUri(`${BASE_URL_GO}/privacy`, {type: 'pdf'})}><Text
-                        style={[globalStyles.text, {paddingHorizontal: 10}]}>Datenschutzerklärung</Text></TouchableOpacity>
                 </View>
             </View>
-        </View>
-
+        </KeyboardAvoidingView>
     )
 }
 
