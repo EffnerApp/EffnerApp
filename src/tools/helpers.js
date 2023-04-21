1658edebe094895121bb7a5743e962d5edad2cc2
Yannick <yan@effner.app>
2023-04-21 14:20:00 +0200

    added the ability to provide the Splash screen with a "screen" option that is then passed to the main screen, which uses this as the default screen to open. made the recent classes list add the current class by default if there hasn't been a recent class saved before made saveAndLoadClass in the helper be able to pass the screen option to the splash screen when provided with a next parameter made the "current class" badge in the title component use the theme's color rather than a pre-set one improved the "no recent classes" message made some style changes in the recent class popup

--- src/tools/helpers.js
+++ src/tools/helpers.js
@@ -35,20 +35,23 @@ const navigateTo = (navigation, to, params = {}) => {
     }));
 }
 
-const saveAndLoadClass = (navigation, c) => {
+const saveAndLoadClass = (navigation, c, next = null) => {
     save('APP_CLASS', c)
         .then(() => {
             saveRecentClass(c)
                 .catch(err => console.log(err))
-                .finally(() => navigateTo(navigation, 'Splash'))
+                .finally(() => navigateTo(navigation, 'Splash', {nextScreen: next}))
         })
         .catch(({message, response}) => showToast('Error while performing class change.', response?.data?.status?.error || message, 'error'));
 }
 
 const saveRecentClass = (c) => {
     return new Promise((resolve, reject) => {
-        load("RECENT_CLASSES").then(recent => {
-            if (! recent) recent = []
+        load("RECENT_CLASSES").then(async recent => {
+            if (! recent) {
+                let oldClass = await load("APP_CLASS")
+                recent = [oldClass] // recent = current
+            }
 
             if (recent.includes(c))
                 recent = recent.filter(x => x !== c)
